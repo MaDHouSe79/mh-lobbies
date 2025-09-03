c57ff518249489b70503edadf44e809d39ba67c4
MaDHouSe <madhouse1979@gmail.com>
2025-09-03 13:37:36 +0200

    upload
    ...

--- core/bridge/inventory/client.lua
+++ core/bridge/inventory/client.lua
@@ -1,77 +1,77 @@
 local Functions = {}
 Functions.Inventory = {}
 
-if GetResourceState("es_extended") ~= 'missing' then
-    if GetResourceState("ox_inventory") ~= 'missing' then
-        Functions.Inventory = {
-            HasItem = function(item, amount)
-                if PlayerData and type(PlayerData.inventory) == "table" then
-                    for _, itemData in pairs(PlayerData.inventory) do
-                        if itemData.name:lower() == item:lower() then
-                            return true
-                        end
+if GetResourceState("ox_inventory") ~= 'missing' then
+    Functions.Inventory = {
+
+        HasItem = function(item, amount)
+            if PlayerData and type(PlayerData.inventory) == "table" then
+                for _, itemData in pairs(PlayerData.inventory) do
+                    if itemData.name:lower() == item:lower() then
+                        return true
                     end
                 end
-                return false
-            end,
-
-            CountItem = function(item)
-                local count = 0
-                if PlayerData and type(PlayerData.inventory) == "table" then
-                    for _, itemData in pairs(PlayerData.inventory) do
-                        if itemData.name:lower() == item:lower() then
-                            count = count + itemData.amount
-                        end
-                    end
-                end
-                return count
-            end,
-
-            LockInventory = function(state)
-                if state then
-                    LocalPlayer.state:set("inv_busy", true, true) -- lock
-                else
-                    LocalPlayer.state:set("inv_busy", false, true) -- unlock
-                end
-            end,
-
-            RequiredItems = function(items)
-                Notify("You don't have a "..items.."...", "error", 5000)
-            end,
-        }
-    end
-elseif GetResourceState("qb-core") ~= 'missing' then
-    if GetResourceState("qb-inventory") ~= 'missing' then
-        Functions.Inventory = {
-            HasItem = function(item, amount)
-                return exports['qb-inventory']:HasItem(item, amount)
-            end,
-
-            CountItem = function(item)
-                local count = 0
-                if PlayerData and type(PlayerData.items) == "table" then
-                    for _, itemData in pairs(PlayerData.items) do
-                        if itemData.name:lower() == item:lower() then
-                            count = count + itemData.amount
-                        end
-                    end
-                end
-                return count
-            end,
-
-            LockInventory = function(state)
-                if state then
-                    LocalPlayer.state:set("inv_busy", true, true) -- lock
-                else
-                    LocalPlayer.state:set("inv_busy", false, true) -- unlock
-                end
-            end,
-
-            RequiredItems = function(items, bool)
-                TriggerEvent('qb-inventory:client:requiredItems', items, bool)
             end
-        }
-    end
+            return false
+        end,
+
+        CountItem = function(item)
+            local count = 0
+            if PlayerData and type(PlayerData.inventory) == "table" then
+                for _, itemData in pairs(PlayerData.inventory) do
+                    if itemData.name:lower() == item:lower() then
+                        count = count + itemData.amount
+                    end
+                end
+            end
+            return count
+        end,
+
+        LockInventory = function(state)
+            if state then
+                LocalPlayer.state:set("inv_busy", true, true) -- lock
+            else
+                LocalPlayer.state:set("inv_busy", false, true) -- unlock
+            end
+        end,
+
+        RequiredItems = function(items)
+            Notify("You don't have a " .. items .. "...", "error", 5000)
+        end
+
+    }
+elseif GetResourceState("qb-inventory") ~= 'missing' then
+    Functions.Inventory = {
+
+        HasItem = function(item, amount)
+            return exports['qb-inventory']:HasItem(item, amount)
+        end,
+
+        CountItem = function(item)
+            local count = 0
+            if PlayerData and type(PlayerData.items) == "table" then
+                for _, itemData in pairs(PlayerData.items) do
+                    if itemData.name:lower() == item:lower() then
+                        count = count + itemData.amount
+                    end
+                end
+            end
+            return count
+        end,
+
+        LockInventory = function(state)
+            if state then
+                LocalPlayer.state:set("inv_busy", true, true) -- lock
+            else
+                LocalPlayer.state:set("inv_busy", false, true) -- unlock
+            end
+        end,
+
+        RequiredItems = function(items, bool)
+            TriggerEvent('qb-inventory:client:requiredItems', items, bool)
+        end
+        
+    }
 end
 
 -- Do not edit this below
--- core/bridge/inventory/server.lua
+++ core/bridge/inventory/server.lua
@@ -1,122 +1,130 @@
-
 local Functions = {}
 
-if GetResourceState("es_extended") ~= 'missing' then
-    if GetResourceState("ox_inventory") ~= 'missing' then
-        Functions.Inventory = {
+if GetResourceState("ox_inventory") ~= 'missing' then
+    Functions.Inventory = {
 
-            OpenInventory = function(src, type)
-                local inventory = MySQL.query.await('SELECT * FROM ox_inventory WHERE name = ?', {type})[1]
-                local items = {}
-                if inventory ~= nil then
-                    local data = json.decode(inventory.data)
-                    for k, v in pairs(data) do
-                        items[#items + 1] = { v.name, v.count }
+        OpenInventory = function(src, type)
+            local inventory = MySQL.query.await('SELECT * FROM ox_inventory WHERE name = ?', {type})[1]
+            local items = {}
+            if inventory ~= nil then
+                local data = json.decode(inventory.data)
+                for k, v in pairs(data) do
+                    items[#items + 1] = {v.name, v.count}
+                end
+            end
+            local mystash = exports.ox_inventory:CreateTemporaryStash({
+                label = 'Custom Stash',
+                slots = 50,
+                maxWeight = 500000,
+                items = items
+            })
+            TriggerClientEvent('ox_inventory:openInventory', src, 'stash', mystash)
+        end,
+
+        GetInventory = function(src, name)
+            local inventory = MySQL.query.await('SELECT * FROM ox_inventory WHERE name = ?', {name})[1]
+            return {
+                items = json.decode(inventory.data)
+            }
+        end,
+
+        SetInventory = function(src, name, type)
+            return exports.ox_inventory:RegisterStash(name, "Custom Stash", 50, 5000000, false, nil, nil)
+        end,
+
+        AddItem = function(src, item, amount, slot, info, reason)
+            return exports.ox_inventory:AddItem(src, item, amount)
+        end,
+
+        RemoveItem = function(src, item, amount, slot)
+            return exports.ox_inventory:RemoveItem(src, item, amount, slot, false)
+        end,
+
+        HasItem = function(src, items, amount)
+            return exports.ox_inventory:HasItem(src, items, amount)
+        end,
+
+        GetItemByName = function(src, item)
+            return exports.ox_inventory:GetItemByName(src, item)
+        end,
+
+        CountItem = function(src, item)
+            local count = 0
+            Player = QBCore.Functions.GetPlayer(src)
+            if Player and type(Player.PlayerData.inventory) == "table" then
+                for _, itemData in pairs(Player.PlayerData.inventory) do
+                    if itemData.name:lower() == item:lower() then
+                        count = count + itemData.amount
                     end
                 end
-                local mystash = exports.ox_inventory:CreateTemporaryStash({ label = 'Custom Stash', slots = 50, maxWeight = 500000, items = items })
-                TriggerClientEvent('ox_inventory:openInventory', src, 'stash', mystash)
-            end,
+            end
+            return count
+        end
+    }
 
-            GetInventory = function(src, name)
-                local inventory = MySQL.query.await('SELECT * FROM ox_inventory WHERE name = ?', {name})[1]
-                return {items = json.decode(inventory.data)}
-            end,
+elseif GetResourceState("qb-inventory") ~= 'missing' then
+    Functions.Inventory = {
 
-            SetInventory = function(src, name, type)
-                return exports.ox_inventory:RegisterStash(name, "Custom Stash", 50, 5000000, false, nil, nil)
-            end,
+        OpenInventory = function(src, type)
+            return exports['qb-inventory']:OpenInventory(src, type, {
+                label = 'Custom Stash',
+                maxweight = 5000000,
+                slots = 50
+            })
+        end,
 
-            AddItem = function(src, item, amount, slot, info, reason)
-                return exports.ox_inventory:AddItem(src, item, amount)
-            end,
+        GetInventory = function(src, inventory)
+            return exports['qb-inventory']:GetInventory(inventory)
+        end,
 
-            RemoveItem = function(src, item, amount, slot)
-                return exports.ox_inventory:RemoveItem(src, item, amount, slot, false)
-            end,
+        SetInventory = function(src, name, items)
+            return exports['qb-inventory']:SetInventory(name, items)
+        end,
 
-            HasItem = function(src, items, amount)
-                return exports.ox_inventory:HasItem(src, items, amount)
-            end,
+        UpdateStash = function(identifier, items)
+            exports['qb-inventory']:UpdateStash(identifier, items)
+        end,
 
-            GetItemByName = function(src, item)
-                return exports.ox_inventory:GetItemByName(src, item)
-            end,
+        AddItem = function(src, item, amount, slot, info, reason)
+            local addReason = reason or 'No reason specified'
+            exports['qb-inventory']:AddItem(src, item, amount, slot, info, addReason)
+            if type(src) == 'number' then
+                TriggerClientEvent('qb-inventory:client:ItemBox', src, Framework.Shared.Items[item], 'add', amount)
+            end
+        end,
 
-            CountItem = function(src, item)
-                local count = 0
-                Player = QBCore.Functions.GetPlayer(src)
-                if Player and type(Player.PlayerData.inventory) == "table" then
-                    for _, itemData in pairs(Player.PlayerData.inventory) do
-                        if itemData.name:lower() == item:lower() then
-                            count = count + itemData.amount
-                        end
+        RemoveItem = function(src, item, amount, slot)
+            exports['qb-inventory']:RemoveItem(src, item, amount, slot, false)
+            if type(src) == 'number' then
+                TriggerClientEvent('qb-inventory:client:ItemBox', src, Framework.Shared.Items[item], 'remove', amount)
+            elseif type(src) == 'string' then
+                local inventory = exports['qb-inventory']:GetInventory(src)
+                SetInventory(src, src, inventory.items)
+            end
+        end,
+
+        HasItem = function(src, items, amount)
+            return exports['qb-inventory']:HasItem(src, items, amount)
+        end,
+
+        GetItemByName = function(src, item)
+            return exports['qb-inventory']:GetItemByName(src, item)
+        end,
+
+        CountItem = function(src, item)
+            local count = 0
+            Player = GetPlayer(src)
+            if Player and type(Player.PlayerData.items) == "table" then
+                for _, itemData in pairs(Player.PlayerData.items) do
+                    if itemData.name:lower() == item:lower() then
+                        count = count + itemData.amount
                     end
                 end
-                return count
-            end,
-        }
-    end
-elseif GetResourceState("qb-core") ~= 'missing' then
-    if GetResourceState("qb-inventory") ~= 'missing' then
-        Functions.Inventory = {
+            end
+            return count
+        end
+    }
 
-            OpenInventory = function(src, type)
-                return exports['qb-inventory']:OpenInventory(src, type, { label = 'Custom Stash', maxweight = 5000000, slots = 50 })
-            end,
-
-            GetInventory = function(src, inventory)
-                return exports['qb-inventory']:GetInventory(inventory)
-            end,
-
-            SetInventory = function(src, name, items)
-                return exports['qb-inventory']:SetInventory(name, items)
-            end,
-
-            UpdateStash = function(identifier, items)
-                exports['qb-inventory']:UpdateStash(identifier, items)
-            end,
-
-            AddItem = function(src, item, amount, slot, info, reason)
-                local addReason = reason or 'No reason specified'
-                exports['qb-inventory']:AddItem(src, item, amount, slot, info, addReason)
-                if type(src) == 'number' then
-                    TriggerClientEvent('qb-inventory:client:ItemBox', src, Framework.Shared.Items[item], 'add', amount)
-                end
-            end,
-
-            RemoveItem = function(src, item, amount, slot)
-                exports['qb-inventory']:RemoveItem(src, item, amount, slot, false)
-                if type(src) == 'number' then 
-                    TriggerClientEvent('qb-inventory:client:ItemBox', src, Framework.Shared.Items[item], 'remove', amount)
-                elseif type(src) == 'string' then
-                    local inventory = exports['qb-inventory']:GetInventory(src)
-                    SetInventory(src, src, inventory.items)
-                end
-            end,
-
-            HasItem = function(src, items, amount)
-                return exports['qb-inventory']:HasItem(src, items, amount)
-            end,
-
-            GetItemByName = function(src, item)
-                return exports['qb-inventory']:GetItemByName(src, item)
-            end,
-
-            CountItem = function(src, item)
-                local count = 0
-                Player = GetPlayer(src)
-                if Player and type(Player.PlayerData.items) == "table" then
-                    for _, itemData in pairs(Player.PlayerData.items) do
-                        if itemData.name:lower() == item:lower() then
-                            count = count + itemData.amount
-                        end
-                    end
-                end
-                return count
-            end,
-        }
-    end
 end
 
 -- Do not edit this below.
