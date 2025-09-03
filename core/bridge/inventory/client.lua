3c000a8c748d9253d71ece181e8acc45177485ac
MaDHouSe <madhouse1979@gmail.com>
2025-09-03 13:38:19 +0200

    Update client.lua

--- core/bridge/inventory/client.lua
+++ core/bridge/inventory/client.lua
@@ -9,6 +9,7 @@ if GetResourceState("ox_inventory") ~= 'missing' then
                 for _, itemData in pairs(PlayerData.inventory) do
                     if itemData.name:lower() == item:lower() then
                         return true
+                        break
                     end
                 end
             end
